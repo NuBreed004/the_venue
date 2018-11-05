8be4f96ef6bb0299d83407e4d606e5f8701d1ac1
Alexander <nubreed004@gmail.com>
2018-11-05 21:42:31 +0300

    material-ui Buttons and Toolbar added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+
 import Header from './components/header';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-      </div>
-    );
+        <AppBar
+          position="fixed"
+          style={{
+            backgroundColor: '#2f2f2f',
+            boxShadow: 'none',
+            padding: '10px 0px'
+          }}
+          >
+          <Toolbar>
+
+            <div className="header_logo">
+              <div className="font_righteous header_logo_venue">
+                The Venue
+              </div>
+              <div className="header_logo_title">
+                Musical Event
+              </div>
+            </div>
+
+            <IconButton
+                aria-label="Menu"
+                color="inherit"
+            >
+              <MenuIcon>
+
+              </MenuIcon>
+            </IconButton>
+
+          </Toolbar>
+        </AppBar>
+    )
   }
 }
 
