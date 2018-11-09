7d6feb2cf6d0f7c3938cd4161b959fd4f5e254bc
Alexander <nubreed004@gmail.com>
2018-11-09 17:41:50 +0300

    react-reveal (Slide) added for countdown

--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Slide from 'react-reveal/Slide';
+
 
 class TimeUntil extends Component {
 
   state = {
 
   }
-
   renderTimes = () => {
 
   }
 
 
-
-
-
-
   render() {
     return (
+      <Slide left delay={1000}>
       <div className="countdown_wrapper">
         <div className="countdown_top">
           Event Starts In
@@ -30,35 +28,34 @@ class TimeUntil extends Component {
               Days
             </div>
           </div>
-            <div className="countdown_item">
-              <div className="countdown_time">
-                2
-              </div>
-              <div className="countdown_tag">
-                Hours
-              </div>
+          <div className="countdown_item">
+            <div className="countdown_time">
+              2
             </div>
-            <div className="countdown_item">
-              <div className="countdown_time">
-                15
-              </div>
-              <div className="countdown_tag">
-                Minutes
-              </div>
+            <div className="countdown_tag">
+              Hours
             </div>
-              <div className="countdown_item">
-                <div className="countdown_time">
-                  00
-                </div>
-                <div className="countdown_tag">
-                  Seconds
-                </div>
-              </div>
+          </div>
+          <div className="countdown_item">
+            <div className="countdown_time">
+              15
+            </div>
+            <div className="countdown_tag">
+              Minutes
+            </div>
+          </div>
+          <div className="countdown_item">
+            <div className="countdown_time">
+              00
+            </div>
+            <div className="countdown_tag">
+              Seconds
+            </div>
+          </div>
         </div>
       </div>
-    );
+    </Slide>
+  );
   }
-
 }
-
 export default TimeUntil;
